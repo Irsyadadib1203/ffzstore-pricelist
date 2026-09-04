3046ffade3c95906ef612d403df574f4bae691ee
irsyadadib1203 <irsyadadib1234@gmail.com>
2026-09-04 08:08:12 +0700

    ffm

--- app/page.tsx
+++ app/page.tsx
@@ -191,7 +191,7 @@ export default function Page() {
 
     for (const item of products) {
       // Pastikan produk dengan kode FFMX tidak dimasukkan
-      if (item.product_code && item.product_code.toUpperCase().includes("FFMX")) {
+      if (item.product_code && item.product_code.toUpperCase().includes("FFMX") && item.product_code.toUpperCase().includes("FFM")) {
         continue;
       }
 
